4f0934b31910012199ade63bb9fa95f4a8bdd6dc
Cristian Nitescu <nitescuc@hotmail.com>
2020-08-03 12:46:32 +0100

    Added Stop service

--- src/drive_actuators.cpp
+++ src/drive_actuators.cpp
@@ -1,8 +1,10 @@
 #include "ros/ros.h"
 #include "std_msgs/Float64.h"
+#include "car_actuators/Stop.h"
 
 #include <pigpiod_if2.h>
 
+double odom = 0.0, odom_limit = 0.2;
 
 class Servo
 {
@@ -47,6 +49,33 @@ void throttle_callback(const std_msgs::Float64 msg)
     throttle->update(msg.data);
 }
 
+void odom_callback(const std_msgs::Float64 msg)
+{
+    odom = msg.data;
+}
+
+bool handle_stop_command(car_actuators::Stop::Request& req,
+    car_actuators::Stop::Response& res)
+{
+
+    if (req.stop && odom > odom_limit)
+    {
+        throttle->update(-1);
+        ros::Rate loop_rate(100);
+        // wait until stop
+        while (ros::ok() && odom > odom_limit)
+        {
+            ros::spinOnce();
+            loop_rate.sleep();
+        }
+    }
+
+    // Return a response message
+    res.stopped = true;
+
+    return true;
+}
+
 int main(int argc, char** argv)
 {
     // Initialize the process_image node and create a handle to it
@@ -62,6 +91,9 @@ int main(int argc, char** argv)
     // Subscribe to /actuator/drive
     ros::Subscriber steering = n.subscribe("/actuator/steering", 1, steering_callback);
     ros::Subscriber throttle = n.subscribe("/actuator/throttle", 1, throttle_callback);
+
+    ros::ServiceServer service = n.advertiseService("/actuator/stop", handle_stop_command);
+    ros::Subscriber odom = n.subscribe("/odom", 1, odom_callback);
 
     // Handle ROS communication events
     ros::spin();
